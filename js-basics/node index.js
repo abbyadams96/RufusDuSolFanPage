d0c7ce440a7bf6211bfbd70ba6beb8bd540227d1
Abigail Adams <abigail-adams@gmail.com>
2021-02-01 15:58:48 -0800

    commit

---
+++ js-basics/node index.js
@@ -0,0 +1,14 @@
+let name ='Mosh';
+console.log(name);
+
+//cannot be a reserved keyword
+//var names should be meaningful
+//cannot start w a number (1name)
+//cannot contain a spce or (-)
+
+let firstName; // capitalize all words after first, this is not initialized
+let lastName = 'mosh'; //this variable is initialized
+//modern best practice is to initialize variables on seperate lines
+// it is possible to initialize vars on same line
+let firstName ='pit';let lastName ='mosh';
+//variables are case sensitive
